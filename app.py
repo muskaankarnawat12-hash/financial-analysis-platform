d4e5bdb1fb78922e1ba3334ee530578a919fa701
Muskaann Karnawat <muskaannkarnawat@Muskaanns-MacBook-Air.local>
2026-09-08 17:07:20 +0530

    Fix SEC filings import

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import streamlit as st
 
 from ai_analysis import generate_rule_based_analysis
 from data_sources import get_company_data
+from sec_data import get_sec_company_filings
 from exports import create_excel_model, create_pdf_report
 from financial_model import (
     calculate_dcf,
